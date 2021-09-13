4ad1bd73d0e669ab9a5b839e6c60089e455f2b95
Skrebnev Fedor <40490810+Skrebnevf@users.noreply.github.com>
2021-09-14 00:27:30 +0200

    add week forecast

--- lib/widgets/bottom_list_view.dart
+++ lib/widgets/bottom_list_view.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:weather_app/models/weather_forecast_dayli.dart';
+import 'package:weather_app/widgets/forecastCard.dart';
 
 class ButtomListView extends StatelessWidget {
   final AsyncSnapshot<WeatherForecast> snapshot;
@@ -14,6 +15,22 @@ class ButtomListView extends StatelessWidget {
           '7 - Day Weather Forecast'.toUpperCase(),
           style: TextStyle(
               fontSize: 20, color: Colors.black, fontWeight: FontWeight.bold),
+        ),
+        Container(
+          height: 140,
+          padding: EdgeInsets.symmetric(vertical: 16, horizontal: 16),
+          child: ListView.separated(
+              scrollDirection: Axis.horizontal,
+              itemBuilder: (context, index) => Container(
+                    width: MediaQuery.of(context).size.width / 2.7,
+                    height: 160,
+                    color: Colors.black,
+                    child: forecastCard(snapshot, index),
+                  ),
+              separatorBuilder: (context, index) => SizedBox(
+                    width: 8,
+                  ),
+              itemCount: snapshot.data!.list!.length),
         )
       ],
     );
